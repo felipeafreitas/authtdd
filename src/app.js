8b381ee040d6ab82c1b0a808b934500a45dd6fff
Felipe Freitas <afelipe.freitas@gmail.com>
2021-03-21 00:03:04 -0300

    Base

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+class AppController {
+    constructor() {
+        this.express = express();
+
+        this.middleswares();
+        this.routes()
+    }
+
+    middleswares() {
+        this.express.use(express.json());
+    }
+
+    routes() {
+        this.express.use(require('./routes'));
+    }
+}
+module.exports = new AppController().express;
---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+	host: '127.0.0.1',
+	username: 'sincopeiro',
+	password: 'W$k*x5)v)Tx)Wy.',
+	database: 'nodeauth',
+	dialect: 'postgres',
+	operatorsAliases: false,
+	logging: false,
+	define: {
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true,
+	},
+};
